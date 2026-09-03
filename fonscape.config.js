013e17f7c697f0245a4672e062eb68613a1e60c9
Loverpyh <150495371+Loverpyh@users.noreply.github.com>
2026-09-03 20:56:25 +0800

    Update fonscape.config.js

--- fonscape.config.js
+++ fonscape.config.js
@@ -2,35 +2,35 @@ const defaultHeroImage = "/fonscape/hero-white.svg";
 
 const siteConfig = {
   language: "zh-CN",
-  title: "我的博客",
+  title: "Adoretheall",
   description: "网站简介",
   postCategories: ["随笔", "评谈", "记录", "笔记", "指南"],
-  showPoems: false,
-  showMusic: false,
+  showPoems: true,
+  showMusic: true,
   home: {
     eyebrow: "PERSONAL BLOG",
     title: "我的博客",
-    description: "网站简介",
+    description: "人性的背后是白云苍狗，愿你我都做生活的高手",
   },
   author: {
     name: "博客作者",
     avatar: "",
     avatarAlt: "博客作者头像",
     tagline: "个人签名",
-    introduction: "个人简介",
+    introduction: "人性的背后是白云苍狗，愿你我都做生活的高手",
     interests: [],
     channels: {
       github: {
         label: "",
-        url: "",
+        url: "https://github.com/Loverpyh/",
       },
       bilibili: {
         label: "",
-        url: "",
+        url: "https://space.bilibili.com/507685329?spm_id_from=333.1007.0.0",
       },
       x: {
         label: "",
-        url: "",
+        url: "https://x.com/adoretheall_pyh",
       },
       email: {
         address: "",
